ba85e8efbbe8f5e66929b04db6d55e6616446a9c
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-14 11:39:38 -0300

    enviando email com spring boot

--- src/main/java/com/ms/msemail/controllers/EmailController.java
+++ src/main/java/com/ms/msemail/controllers/EmailController.java
@@ -1,12 +1,29 @@
 package com.ms.msemail.controllers;
 
+import com.ms.msemail.dtos.EmailDto;
+import com.ms.msemail.models.EmailModel;
 import com.ms.msemail.services.EmailService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
 }
